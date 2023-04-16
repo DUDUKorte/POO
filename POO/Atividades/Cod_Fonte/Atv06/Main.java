ac71ec7fa8abe07ea28560a76a3575a0ef858947
esquilo korte <dudu.korte@gmail.com>
2023-04-16 20:22:07 -0300

    Add files via upload

--- POO/Atividades/Cod_Fonte/Atv06/Main.java
+++ POO/Atividades/Cod_Fonte/Atv06/Main.java
@@ -1,21 +1,35 @@
 package Atividades.Cod_Fonte.Atv06;
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.Scanner; //importa biblioteca Scanner
 
 public class Main {
  
-    public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in); //inicializa o scanner
 
-        String T = scan.nextLine();
-        
-        
-        if(T.length() <= 140){
-            System.out.println("TWEET");
-        }else{
-            System.out.println("MUTE");
-        }
- 
+        System.out.print("Informe um valor fixo do depósito mensal: ");
+        double depositoMensal = scan.nextDouble(); //solicita um valor para o usuário
+
+        System.out.print("Informe o montante desejado: ");
+        double montante = scan.nextDouble();
+
+        double months = calcula(montante, depositoMensal); //chama o método calcula, com as variáveis necessárias para realizar o cálculo e retorna pra variável months
+
+        System.out.println("Mêses necessários: " + months); //exibe o resultado na tela
+
+        scan.close(); //encerra o Scanner
     }
- 
+
+    public static double calcula(double montante, double deposit){ //declara o método calcula, que retorna um valor double
+        double months = 0;
+
+        while(true){ //sistema de repetição while inicializado
+            if(montante <= 0){ //verifica se o montante é menor ou igual a 0
+                break; //realiza um break para finalizar/interromper o while
+            }
+            months += 1; //incrementa variável months em 1 
+            montante -= deposit; //reduz o depósito do montante
+            deposit *=  1.05; //adiciona 5% de juros ao depósito
+        }
+        return months; //retorna o valor de months
+    }
 }
